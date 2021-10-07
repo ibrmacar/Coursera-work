e79d20118529d89c0875ae803965b8393f6886ea
Your Name <you@example.com>
2021-10-07 23:24:29 +0300

    Solutions

--- Module4-solution/script.js
+++ Module4-solution/script.js
@@ -1,13 +1,21 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <title>Module 4 Solution Starter</title>
-  <script src="SpeakHello.js"></script>
-  <script src="SpeakGoodBye.js"></script>
-  <script src="script.js"></script>
-</head>
-<body>
-  <h1>Module 4 Solution Starter</h1>
-</body>
-</html>
+var names=new Array();
+names[0]="Kenzie";
+names[1]="James";
+names[2]="jonhatan";
+names[3]="Jeremy";
+names[4]="Will";
+names[5]="Smith";
+names[6]="jim";
+names[7]="Scarlet";
+names[8]="Zona";
+names[9]="jessica";
+
+
+for (var i = 0; i < names.length; i++) {
+	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
+        console.log("Goodbye "+ names[i])
+	}
+	else{
+		console.log("Hello "+ names[i])
+	}
+}
